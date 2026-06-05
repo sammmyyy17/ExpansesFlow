e6eea6b8aa05392225d47b78f9b2215c4ca6187d
sammmyyy17 <156781976+sammmyyy17@users.noreply.github.com>
2026-06-05 10:20:20 +0530

    Update settings.py

--- salary/settings.py
+++ salary/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'salary.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
