c85d51bdfcd4946a10270e9bb8686d23c53af36e
sammmyyy17 <156781976+sammmyyy17@users.noreply.github.com>
2026-06-05 10:32:13 +0530

    Update settings.py

--- salary/settings.py
+++ salary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os               # <-- ADD THIS EXACT LINE HERE!
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
